b486e15f3acdad912160adb60fc99300f71c573f
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2022-04-13 22:06:20 -0300

    feat: passar dados de component filho para o pai

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,55 @@
 import React, { FormEvent, useCallback, useEffect, useRef } from 'react';
-import logo from './logo.svg'; 
+import logo from './logo.svg';
 import Input from './components/Input';
+import Modal, { ModalHandles } from './components/Modal';
 
 
 function App() {
   const nameInputRef = useRef<HTMLInputElement>(null);
-  const acceptTermsRef = useRef( { value: false});
+  const acceptTermsRef = useRef({ value: false });
+  const modalRef = useRef<ModalHandles>(null)
   // useEffect(()=> {
   //   nameInputRef.current?.focus();
   // }, [])
 
-  const handleSubmit = useCallback((e :FormEvent) => {
+  const handleSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
     //nameInputRef.current?.focus();
     alert(nameInputRef.current?.value)
   }, []);
 
   const handleAccepTerms = useCallback(() => {
-    console.log(acceptTermsRef.current.value= !acceptTermsRef.current.value);
+    acceptTermsRef.current.value = !acceptTermsRef.current.value;
   }, []);
 
+  const handleopenModal = useCallback(() => {
+   modalRef.current?.openModal();
+  }, []); 
+  
   return (
-    <form onSubmit={handleSubmit}>
-    <div className="App"> 
-        <Input 
-          name="name" 
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="name"
           label="Nome completo"
           ref={nameInputRef}
-          />
+        />
 
-            <button type="button" onClick={handleAccepTerms}>
-                Aceitar termos
-              </button>
+        <button type="button" onClick={handleAccepTerms}>
+          Aceitar termos
+        </button>
 
-          <button type='submit' >Realizar foco</button>
+        <button type='submit' >Realizar foco</button>
+
+
+        
+        
+      </form>
+
+      <button onClick={handleopenModal}> Abrir modal </button>
+
+      <Modal ref={modalRef} />
     </div>
-    </form>
   );
 }
 
