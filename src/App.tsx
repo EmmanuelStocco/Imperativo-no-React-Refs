063d1b9cd2af4fa1cac1f604e18b0b6de093b127
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2022-04-13 21:19:46 -0300

    feat: second

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import logo from './logo.svg'; 
 
 function App() {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+
+  // useEffect(()=> {
+  //   nameInputRef.current?.focus();
+  // }, [])
+
+  const handleFocus = useCallback(() => {
+    nameInputRef.current?.focus();
+  }, []);
+
   return (
     <div className="App"> 
-        <input type="text" />
+        <input 
+          type="text"
+          placeholder='Digite seu nome'
+          ref={nameInputRef} 
+          />
+
+          <button onClick={handleFocus}>Realizar foco</button>
     </div>
   );
 }
