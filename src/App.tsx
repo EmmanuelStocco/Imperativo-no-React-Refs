7dff3869976adf9fa873e31b95fc9bc34ecda9b9
EmmanuelStocco <emmanuelrolimstocco@gmail.com>
2022-04-13 21:27:47 -0300

    feat: refs att dados sem re-renderizar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,27 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { FormEvent, useCallback, useEffect, useRef } from 'react';
 import logo from './logo.svg'; 
+
+
 
 function App() {
   const nameInputRef = useRef<HTMLInputElement>(null);
-
+  const acceptTermsRef = useRef( { value: false});
   // useEffect(()=> {
   //   nameInputRef.current?.focus();
   // }, [])
 
-  const handleFocus = useCallback(() => {
-    nameInputRef.current?.focus();
+  const handleSubmit = useCallback((e :FormEvent) => {
+    e.preventDefault();
+    //nameInputRef.current?.focus();
+    alert(nameInputRef.current?.value)
+  }, []);
+
+  const handleAccepTerms = useCallback(() => {
+    console.log(acceptTermsRef.current.value= !acceptTermsRef.current.value);
   }, []);
 
   return (
+    <form onSubmit={handleSubmit}>
     <div className="App"> 
         <input 
           type="text"
@@ -20,8 +29,13 @@ function App() {
           ref={nameInputRef} 
           />
 
-          <button onClick={handleFocus}>Realizar foco</button>
+            <button type="button" onClick={handleAccepTerms}>
+                Aceitar termos
+              </button>
+
+          <button type='submit' >Realizar foco</button>
     </div>
+    </form>
   );
 }
 
